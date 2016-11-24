66562c39a1ffc7da59a53fb93f2e75b2cca2be18
Javaheri, Cameron <cameron.javaheri@mines.sdsmt.edu>
2016-11-24 16:34:47 -0600

    Made breadth first search function.

--- Monster/monster.cpp
+++ Monster/monster.cpp
@@ -10,12 +10,27 @@ Monster::Monster(Playfield* p)
 // YOUR MAIN TASK IS TO RE-IMPLEMENT THIS METHOD TO DO SOMETHING INTELLIGENT
 const Site* Monster::move() 
 {
+   bool **marked = nullptr;
+   Site* **prev = nullptr;
+   int ** dist = nullptr;
    const Site* monster   = playfield->getMonsterSite();
    const Site* player = playfield->getPlayerSite();
    const Site* test;
+   int i;
+   int j;
    cout << "Monster[i][j] " << monster->i() << " " << monster->j() << endl;
    cout << "PLayer[i][j] " << player->i() << " " << player->j() << endl;
    cout << endl << endl;
+
+	allocateStorage(marked, prev, dist);
+
+	bfs(marked, prev, dist, monster);
+
+	i = player->i();
+	j = player->j();
+
+	test = prev[i][j];
+	cout << test->i() << " " << test->j() << endl;
 
 	if (playfield->isRoom(monster) == true)
 	{
@@ -27,6 +42,7 @@ const Site* Monster::move()
 		test = moveCorridor(monster, player);
 		return test;
 	}
+
 
    return nullptr;
 
@@ -289,4 +305,216 @@ const Site* Monster::moveCorridor(const Site* monster, const Site* player)
 		}
 		test = monster;
 		return test;
+}
+
+
+void Monster::bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster)
+{
+	queue< Site*> myqueue;
+	bool isRoom;
+	Site* monsterSite = new Site(monster->i(), monster->j());
+	Site* temp;
+
+	// Mark the initial site(monster's site) as visited.
+	marked[monster->i()][monster->j()] = true;
+	dist[monster->i()][monster->j()] = 0;
+	prev[monster->i()][monster->j()] = monsterSite;
+	myqueue.push(monsterSite);
+
+	// While the queue is not empty, visit all adjacent vertices.
+	while (!myqueue.empty())
+	{
+		temp = myqueue.front();
+		myqueue.pop();
+		isRoom = playfield->isRoom(temp);
+		// Check all 8 adjacent vertices to temp.
+		if (isRoom)
+			checkAdjacentRoomSquares(marked, prev, dist, myqueue, temp);
+
+
+		// Temp must be a corridor site, so check all 4 adjacent vertices.
+		else
+			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp);
+
+	}
+
+}
+
+void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp)
+{
+			int i;
+			int j;
+			Site* addSite = nullptr;
+			i = temp->i();
+			j = temp->j();
+			if (!playfield->isWall(i - 1, j))
+			{
+				if (playfield->isRoom(i - 1, j) || playfield->isCorridor(i - 1, j))
+				{
+					if (!marked[i - 1][j])
+					{
+						marked[i - 1][j] = true;
+						dist[i - 1][j] = dist[i][j] + 1;
+						prev[i - 1][j] = temp;
+						addSite = new Site(i - 1, j);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+			if (!playfield->isWall(i + 1, j))
+			{
+				if (playfield->isRoom(i + 1, j) || playfield->isCorridor(i + 1, j))
+				{
+					if (!marked[i + 1][j])
+					{
+						marked[i + 1][j] = true;
+						dist[i + 1][j] = dist[i][j] + 1;
+						prev[i + 1][j] = temp;
+						addSite = new Site(i + 1, j);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+			if (!playfield->isWall(i, j + 1))
+			{
+				if (playfield->isRoom(i, j + 1) || playfield->isCorridor(i, j + 1))
+				{
+					if (!marked[i][j + 1])
+					{
+						marked[i][j + 1] = true;
+						dist[i][j + 1] = dist[i][j] + 1;
+						prev[i][j + 1] = temp;
+						addSite = new Site(i, j + 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+			if (!playfield->isWall(i, j - 1))
+			{
+				if (playfield->isRoom(i, j - 1) || playfield->isCorridor(i, j - 1))
+				{
+					if (!marked[i][j - 1])
+					{
+						marked[i][j - 1] = true;
+						dist[i][j - 1] = dist[i][j] + 1;
+						prev[i][j - 1] = temp;
+						addSite = new Site(i, j - 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+}
+
+void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp)
+{
+
+			Site* addSite = nullptr;
+			int i;
+			int j;
+			i = temp->i();
+			j = temp->j();
+			if (!playfield->isWall(i + 1, j + 1))
+			{
+				if (playfield->isRoom(i + 1, j + 1))
+				{
+					if (!marked[i + 1][j + 1])
+					{
+						marked[i + 1][j + 1] = true;
+						dist[i + 1][j + 1] = dist[i][j] + 1;
+						prev[i + 1][j + 1] = temp;
+						addSite = new Site(i + 1, j + 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+			if (!playfield->isWall(i + 1, j - 1))
+			{
+				if (playfield->isRoom(i + 1, j - 1))
+				{
+					if (!marked[i + 1][j - 1])
+					{
+						marked[i + 1][j - 1] = true;
+						dist[i + 1][j - 1] = dist[i][j] + 1;
+						prev[i + 1][j - 1] = temp;
+						addSite = new Site (i + 1, j - 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+			if (!playfield->isWall(i - 1, j + 1))
+			{
+				if (playfield->isRoom(i - 1, j + 1))
+				{
+					if (!marked[i - 1][j + 1])
+					{
+						marked[i - 1][j + 1] = true;
+						dist[i - 1][j + 1] = dist[i][j] + 1;
+						prev[i - 1][j + 1] = temp;
+						addSite = new Site(i - 1, j + 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+
+
+
+			if (!playfield->isWall(i - 1, j - 1))
+			{
+				if (playfield->isRoom(i - 1, j - 1))
+				{
+					if (!marked[i - 1][j - 1])
+					{
+						marked[i - 1][j - 1] = true;
+						dist[i - 1][j - 1] = dist[i][j] + 1;
+						prev[i - 1][j - 1] = temp;
+						addSite = new Site(i - 1, j - 1);
+						myqueue.push(addSite);
+					}
+				}
+			}
+
+			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp);
+
+}
+
+void Monster::allocateStorage(bool **&marked, Site* **&prev, int **&dist)
+{
+	int i;
+	int j;
+
+	dist = new (nothrow) int *[N];
+	for (i = 0; i < N; i++)
+		dist[i] = new (nothrow) int [N];
+
+	prev = new (nothrow) Site* *[N];
+	for (i = 0; i < N; i++)
+		prev[i] = new (nothrow) Site* [N];
+
+
+	marked = new (nothrow) bool *[N];
+	for (i = 0; i < N; i++)
+		marked[i] = new (nothrow) bool [N];
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			marked[i][j] = false;
+
+
 }
--- Monster/monster.h
+++ Monster/monster.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "site.h"
 #include "playfield.h"
+#include <queue>
 
 class Monster 
 {
@@ -15,6 +16,12 @@ class Monster
        const Site* move();
        const Site* moveRoom(const Site* monster, const Site* player);
        const Site* moveCorridor(const Site* monster, const Site* player);
+       void bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster);
+       void allocateStorage(bool **&marked, Site* **&prev, int **&dist);
+       void checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp);
+
+
+	void checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp);
 };
 
 #endif
