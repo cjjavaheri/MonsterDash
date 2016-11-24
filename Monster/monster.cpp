2af41c58753cf3ce3bb8c3b7da0c1cd2b8c7e84a
Javaheri, Cameron <cameron.javaheri@mines.sdsmt.edu>
2016-11-24 05:07:24 -0600

    Made 2 seperate functions. One for corridors, and one for room sites.

--- Monster/monster.cpp
+++ Monster/monster.cpp
@@ -19,7 +19,45 @@ const Site* Monster::move()
 
 	if (playfield->isRoom(monster) == true)
 	{
-		// Monster's row is smaller than player's row
+		test = moveRoom(monster, player);
+		return test;
+	}
+	if (playfield->isCorridor(monster) == true)
+	{
+		test = moveCorridor(monster, player);
+		return test;
+	}
+
+   return nullptr;
+
+/*
+   int myI = monster->i();
+   int myJ = monster->j();
+
+   vector<const Site *> valid;
+   for (int i = -1; i < 2; i++)
+   {      
+      for (int j = -1; j < 2; j++)
+      {
+         const Site* site = new Site(myI+i, myJ+j);
+         if (playfield->isLegalMove(monster, site))
+            valid.push_back(site);
+      }
+   }   
+
+   const Site* ret = (valid.size() == 0) ? nullptr : valid[rand() % valid.size()];
+   return ret;
+
+*/
+}
+
+
+
+Site* Monster::moveRoom(const Site* monster, const Site* player)
+{
+	Site* test = nullptr;
+
+			// Monster's row is smaller than player's row
 		if (monster->i() < player->i())
 		{
 			//Monster's column is smaller than player's column
@@ -30,11 +68,36 @@ const Site* Monster::move()
 				{
 					return test;
 				}
+
+				test = new Site(monster->i() + 1, monster->j());
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+				
+				test = new Site(monster->i() , monster->j() + 1);
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+				
 			}
 			//Monster's column is greater than player's column
 		        if (monster->j() > player->j())
 			{
 				test = new Site(monster->i() + 1, monster->j() - 1);
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+
+				test = new Site(monster->i() + 1, monster->j());
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+
+				test = new Site(monster->i(), monster->j() - 1);
 				if (playfield->isLegalMove(monster, test))
 				{
 					return test;
@@ -61,11 +124,35 @@ const Site* Monster::move()
 				{
 					return test;
 				}
+
+				test = new Site(monster->i() - 1, monster->j());
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+
+				test = new Site(monster->i(), monster->j() + 1);
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
 			}
 			//Monster's column is greater than player's column
 		        if (monster->j() > player->j())
 			{
 				test = new Site(monster->i() - 1, monster->j() - 1);
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+
+				test = new Site(monster->i() - 1, monster->j());
+				if (playfield->isLegalMove(monster, test))
+				{
+					return test;
+				}
+
+				test = new Site(monster->i(), monster->j() - 1);
 				if (playfield->isLegalMove(monster, test))
 				{
 					return test;
@@ -84,7 +171,7 @@ const Site* Monster::move()
 		// Monster's row is equal to player's row
 		else
 		{
-					//Monster's column is smaller than player's column
+			//Monster's column is smaller than player's column
 			if (monster->j() < player->j())
 			{
 				test = new Site(monster->i(), monster->j() + 1);
@@ -112,27 +199,85 @@ const Site* Monster::move()
 				}
 			}
 		}
-	}
 
-   return nullptr;
+}
 
-/*
-   int myI = monster->i();
-   int myJ = monster->j();
+Site* Monster::moveCorridor(const Site* monster, const Site* player)
+{
+	Site* test = nullptr;
+			// Monster's row is smaller than player's row
+		if (monster->i() < player->i())
+		{
+			test = new Site(monster->i() + 1, monster->j());
+			if (playfield->isLegalMove(monster, test))
+				return test;
+			
+			// Monster's column is smaller than player's column
+			if (monster->j() < player->j())
+			{
+				test = new Site(monster->i(), monster->j() + 1 );
+				if (playfield->isLegalMove(monster, test))
+					return test;
+				
+			}
+			//Monster's column is greater than player's column
+			if (monster->j() > player->j())
+			{
+				test = new Site(monster->i(), monster->j() - 1);
+				if (playfield->isLegalMove(monster, test))
+					return test;
+				
+			}
+		}
+		//Monster's row is greater than player's row
+	       if (monster->i() > player->i())
+		{
+			test = new Site(monster->i() - 1, monster->j());
+			if (playfield->isLegalMove(monster, test))
+				return test;
 
-   vector<const Site *> valid;
-   for (int i = -1; i < 2; i++)
-   {      
-      for (int j = -1; j < 2; j++)
-      {
-         const Site* site = new Site(myI+i, myJ+j);
-         if (playfield->isLegalMove(monster, site))
-            valid.push_back(site);
-      }
-   }   
+			// Monster's column is smaller than player's column
+			if (monster->j() < player->j())
+			{
+				test = new Site(monster->i(), monster->j() + 1 );
+				if (playfield->isLegalMove(monster, test))
+					return test;
+				
+			}
+			//Monster's column is greater than player's column
+			if (monster->j() > player->j())
+			{
+				test = new Site(monster->i(), monster->j() - 1);
+				if (playfield->isLegalMove(monster, test))
+					return test;
+				
+			}
+		}
+		//Monster's row is equal to player's row
+		if (monster->i() == player->i())
+		{
+			//Monster's column is smaller than player's column
+			if (monster->j() < player->j())
+			{
+				test = new Site(monster->i(), monster->j() + 1);
+				if (playfield->isLegalMove(monster, test))
+					return test;
+			}
+			//Monster's column is greater than player's column
+			if (monster->j() > player->j())
+			{
+				test = new Site(monster->i(), monster->j() - 1);
+				if (playfield->isLegalMove(monster, test))
+					return test;
+			}
 
-   const Site* ret = (valid.size() == 0) ? nullptr : valid[rand() % valid.size()];
-   return ret;
+			//Monster's column is equal to player's column
+			if (monster->j() == player->j())
+			{
+				test = new Site(monster->i(), monster->j());
+				if (playfield->isLegalMove(monster, test))
+					return test;
+			}
+		}
 
-*/
 }
