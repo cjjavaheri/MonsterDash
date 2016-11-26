0d242d45c591b36789d4b9c8230618e3c93de2b2
Javaheri, Cameron <cameron.javaheri@mines.sdsmt.edu>
2016-11-26 00:17:25 -0600

    Freed up all memory that was allocated (except for the returning site pointer from move).

--- Monster/monster.cpp
+++ Monster/monster.cpp
@@ -22,18 +22,21 @@ const Site* Monster::move()
    const Site* player = playfield->getPlayerSite();
    Site* nextMove;
    const Site* tryMove;
+   vector<Site*> allocatedMemory;
    cout << "Monster[i][j] " << monster->i() << " " << monster->j() << endl;
    cout << "PLayer[i][j] " << player->i() << " " << player->j() << endl;
    cout << endl << endl;
 
 	allocateStorage(marked, prev, dist);
 
-	bfs(marked, prev, dist, monster);
+	bfs(marked, prev, dist, monster, allocatedMemory);
 
-	nextMove = getNextMove(marked, prev, dist, monster, player);
+	nextMove = getNextMove(marked, prev, dist, monster, player, allocatedMemory);
 
 	tryMove = new Site(nextMove->i(), nextMove->j());
-	deallocateStorage(marked, prev, dist);
+
+	deallocateStorage(marked, prev, dist, allocatedMemory);
+
 	if (playfield->isLegalMove(monster, tryMove))
 		return tryMove;
 
@@ -42,7 +45,7 @@ const Site* Monster::move()
 
 
 
- Site* Monster::getNextMove(bool **&marked, Site* **&prev, int **&dist, const Site* monster, const Site* player)
+ Site* Monster::getNextMove(bool **&marked, Site* **&prev, int **&dist, const Site* monster, const Site* player, vector<Site*> &allocatedMemory)
 {
 	unsigned int i;
 	unsigned int j;
@@ -70,6 +73,7 @@ const Site* Monster::move()
 	else
 	{
 		nextMove = new Site(player->i(), player->j());
+		allocatedMemory.push_back(nextMove);
 		return nextMove;
 	}
 
@@ -77,7 +81,7 @@ const Site* Monster::move()
 }
 
 
-void Monster::bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster)
+void Monster::bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster, vector<Site*> &allocatedMemory)
 {
 	queue< Site*> myqueue;
 	bool isRoom;
@@ -98,20 +102,20 @@ void Monster::bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monste
 		isRoom = playfield->isRoom(temp);
 		// Check all 8 adjacent vertices to temp.
 		if (isRoom)
-			checkAdjacentRoomSquares(marked, prev, dist, myqueue, temp);
+			checkAdjacentRoomSquares(marked, prev, dist, myqueue, temp, allocatedMemory);
 
 
 		// Temp must be a corridor site, so check all 4 adjacent vertices.
 		else
-			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp);
+			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp, allocatedMemory);
 
 	}
 
-	delete monsterSite;
+	allocatedMemory.push_back(monsterSite);
 
 }
 
-void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp)
+void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp, vector<Site*> &allocatedMemory)
 {
 			int i;
 			int j;
@@ -128,6 +132,7 @@ void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **di
 						dist[i - 1][j] = dist[i][j] + 1;
 						prev[i - 1][j] = temp;
 						addSite = new Site(i - 1, j);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -145,6 +150,7 @@ void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **di
 						dist[i + 1][j] = dist[i][j] + 1;
 						prev[i + 1][j] = temp;
 						addSite = new Site(i + 1, j);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -162,6 +168,7 @@ void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **di
 						dist[i][j + 1] = dist[i][j] + 1;
 						prev[i][j + 1] = temp;
 						addSite = new Site(i, j + 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -179,6 +186,7 @@ void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **di
 						dist[i][j - 1] = dist[i][j] + 1;
 						prev[i][j - 1] = temp;
 						addSite = new Site(i, j - 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -186,7 +194,7 @@ void Monster::checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **di
 
 }
 
-void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp)
+void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp, vector<Site*> &allocatedMemory)
 {
 
 			Site* addSite = nullptr;
@@ -204,6 +212,7 @@ void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, 
 						dist[i + 1][j + 1] = dist[i][j] + 1;
 						prev[i + 1][j + 1] = temp;
 						addSite = new Site(i + 1, j + 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -221,6 +230,7 @@ void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, 
 						dist[i + 1][j - 1] = dist[i][j] + 1;
 						prev[i + 1][j - 1] = temp;
 						addSite = new Site (i + 1, j - 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -238,6 +248,7 @@ void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, 
 						dist[i - 1][j + 1] = dist[i][j] + 1;
 						prev[i - 1][j + 1] = temp;
 						addSite = new Site(i - 1, j + 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
@@ -256,18 +267,20 @@ void Monster::checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, 
 						dist[i - 1][j - 1] = dist[i][j] + 1;
 						prev[i - 1][j - 1] = temp;
 						addSite = new Site(i - 1, j - 1);
+						allocatedMemory.push_back(addSite);
 						myqueue.push(addSite);
 					}
 				}
 			}
 
-			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp);
+			checkAdjacentCorridorSquares(marked, prev, dist, myqueue, temp, allocatedMemory);
 
 }
 
-void Monster::deallocateStorage(bool **&marked, Site* **&prev, int **&dist)
+void Monster::deallocateStorage(bool **&marked, Site* **&prev, int **&dist, vector<Site*> &allocatedMemory)
 {
 	int i;
+	vector<Site*>::iterator it;
 
 	for (i = 0; i < N; i++)
 		delete[] marked[i];
@@ -283,6 +296,13 @@ void Monster::deallocateStorage(bool **&marked, Site* **&prev, int **&dist)
 		delete[] dist[i];
 
 	delete[] dist;
+
+	it = allocatedMemory.begin();
+	while (it != allocatedMemory.end())
+	{
+		delete (*it);
+		it++;
+	}
 
 }
 
--- Monster/monster.h
+++ Monster/monster.h
@@ -15,16 +15,16 @@ class Monster
        Monster(Playfield* play);
 	~Monster();
        const Site* move();
-       void bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster);
+       void bfs(bool **&marked, Site* **&prev, int **&dist, const Site* monster, vector<Site*> &allocatedMemory);
        void allocateStorage(bool **&marked, Site* **&prev, int **&dist);
-       void deallocateStorage(bool **&marked, Site* **&prev, int **&dist);
-       void checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp);
+       void deallocateStorage(bool **&marked, Site* **&prev, int **&dist, vector<Site*> &allocatedMemory);
+       void checkAdjacentRoomSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp, vector<Site*> &allocatedMemory);
 
 
-	void checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp);
+	void checkAdjacentCorridorSquares(bool **marked, Site* **prev, int **dist, queue<Site*> &myqueue, Site* temp, vector<Site*> &allocatedMemory);
 
 
-	 Site* getNextMove(bool **&marked, Site* **&prev, int **&dist, const Site* monster, const Site* player);
+	 Site* getNextMove(bool **&marked, Site* **&prev, int **&dist, const Site* monster, const Site* player, vector<Site*> &allocatedMemory);
 };
 
 #endif
